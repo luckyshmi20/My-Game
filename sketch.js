69a052867c477a80507fb71c9da3b7803a6bc932
luckyshmi20 <101426022+luckyshmi20@users.noreply.github.com>
2022-12-19 13:48:19 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var input
+
+function setup(){
+    createCanvas(1580,710); 
+    textSize(80);
+    textStyle(ITALIC);
+    fill("black");
+    text("Welcome to the general knowledge quiz", 100, 100);
+    this.input = createInput("Enter Your Name Here....")
+}
+function draw(){
+  //  background("lilac");
+}
